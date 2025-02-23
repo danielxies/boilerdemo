40ff8572a2ade75f2fd75615334c93bff06df238
danielemporia <xie476@purdue.edu>
2025-02-23 02:51:03 -0500

    final logo change alnding

--- app/page.tsx
+++ app/page.tsx
@@ -133,7 +133,7 @@ export default function LandingPage() {
               <AnimatedGradientText>
                 🎉 <hr className="mx-2 h-4 w-px shrink-0 bg-gray-300" />{" "}
                 <span className={`${vastago.className} inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`}>
-                  introducing mockmate
+                  introducing MockMade
                 </span>
               </AnimatedGradientText>
             </div>
